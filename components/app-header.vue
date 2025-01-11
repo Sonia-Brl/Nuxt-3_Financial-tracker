a0bfcf8f26ed75228944dc731e52c3dc75a21bef
Sonia-Brl <brunelsonia@gmail.com>
2025-01-11 14:11:44 +0100

    Init home page

---
+++ components/app-header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="flex justify-between items-center p-4">
+    <NuxtLink to="/" class="text-xl font-bold"> Financial Tracker </NuxtLink>
+    <div>
+      <UAvatar
+        src="https://avatars.githubusercontent.com/u/69713925?v=4&size=64"
+        alt="Avatar"
+      />
+    </div>
+  </header>
+</template>
