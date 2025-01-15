5df69356bcbafb9b343212af09d2d2478663063e
Sonia-Brl <brunelsonia@gmail.com>
2025-01-15 19:37:13 +0100

    Add Logout logic and user menu dropdown

--- components/app-header.vue
+++ components/app-header.vue
@@ -2,7 +2,48 @@
   <header class="flex justify-between items-center p-4">
     <NuxtLink to="/" class="text-xl font-bold"> Financial Tracker </NuxtLink>
     <div>
-      <UAvatar src="https://avatars.githubusercontent.com/u/69713925?v=4&size=64" alt="Avatar" />
+      <UDropdown :items="items" :ui="{ item: { disabled: 'cursor-text select-text' }, width: 'w-64' }" v-if="user">
+        <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
+        <template #account="{ item }">
+          <div class="text-left">
+            <p>
+              Signed in as
+            </p>
+            <p class="font-medium text-gray-900 dark:text-white">
+              {{ item.label }}
+            </p>
+          </div>
+        </template>
+        <template #item="{ item }">
+          <span class="truncate">{{ item.label }}</span>
+          <UIcon :name="item.icon" class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto" />
+        </template>
+      </UDropdown>
     </div>
   </header>
 </template>
+
+
+<script setup>
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+const items = [
+  [{
+    label: user.value?.email,
+    slot: 'account',
+    disabled: true
+  }], [{
+    label: 'Settings',
+    icon: 'i-heroicons-cog-8-tooth',
+    click: () => console.log('Link to settings in the future')
+  }, {
+    label: 'Sign out',
+    icon: 'i-heroicons-arrow-left-on-rectangle',
+    click: async () => {
+      console.log("a Cliqué");
+      await supabase.auth.signOut();
+      return navigateTo('/login');
+    }
+  }]
+]
+</script>
