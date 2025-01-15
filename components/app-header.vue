bec0c9f54ad585e5475bef6a45685b403e2539c9
Sonia-Brl <brunelsonia@gmail.com>
2025-01-15 18:55:20 +0100

    Add logic to login form

--- components/app-header.vue
+++ components/app-header.vue
@@ -2,10 +2,16 @@
   <header class="flex justify-between items-center p-4">
     <NuxtLink to="/" class="text-xl font-bold"> Financial Tracker </NuxtLink>
     <div>
-      <UAvatar
-        src="https://avatars.githubusercontent.com/u/69713925?v=4&size=64"
-        alt="Avatar"
-      />
+      <UAvatar src="https://avatars.githubusercontent.com/u/69713925?v=4&size=64" alt="Avatar" />
     </div>
   </header>
 </template>
+
+<script setup>
+const user = useSupabaseUser()
+watch(user, (user) => {
+  if (user) {
+    console.log(user)
+  }
+}, { immediate: true })
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -3,12 +3,13 @@
         <template #header>
             Sign in to Finace Tracker
         </template>
-        <form>
+        <form @submit.prevent="handleLogin">
             <UFormGroup label="Email" name="email" class="mb-4" :required="true"
                 help="You wil receive an email with the confirmation link">
-                <UInput type="email" placeholder="email" required />
+                <UInput type="email" placeholder="email" required v-model="email" />
             </UFormGroup>
-            <UButton type="submit" variant="solid" color="black" @click="success = true">Sign-in</UButton>
+            <UButton type="submit" variant="solid" color="black" :loading="pending" :disabled="pending">Sign-in
+            </UButton>
         </form>
     </UCard>
     <UCard v-else>
@@ -16,15 +17,41 @@
             Email has been send
         </template>
         <div class="text-center">
-            <p> We have send a email to <strong>test@test.com</strong> with a link to sign in</p>
+            <p> We have send a email to <strong>{{ email }}</strong> with a link to sign in</p>
             <p><strong>The link will expire in 5 minutes.</strong></p>
         </div>
     </UCard>
 </template>
 
 <script setup>
+
 // http://localhost:3000/login
-
-const success = ref(false);
-
+const success = ref(false)
+const email = ref('')
+const pending = ref(false)
+const toast = useToast()
+const supabase = useSupabaseClient()
+const handleLogin = async () => {
+    pending.value = true
+    try {
+        const { error } = await supabase.auth.signInWithOtp({
+            email: email.value,
+            options: {
+                emailRedirectTo: 'http://localhost:3000'
+            }
+        })
+        if (error) {
+            toast.add({
+                title: 'Error authenticating',
+                icon: 'i-heroicons-exlamation-circle',
+                description: error.message,
+                color: 'red'
+            })
+        } else {
+            success.value = true
+        }
+    } finally {
+        pending.value = false
+    }
+}
 </script>
