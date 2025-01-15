64b322e89d4acdb12ec749c28be62b779acc6cc9
Sonia-Brl <brunelsonia@gmail.com>
2025-01-15 19:10:41 +0100

    Add Login confirmation page

---
+++ pages/confirm.vue
@@ -0,0 +1,13 @@
+<template>
+    <UCard>
+        <template #header>
+            Sign in ...
+        </template>
+        Wait a minute while we sign you in
+    </UCard>
+</template>
+
+<script setup>
+// http://localhost:3000/confirm
+useRedirectIfAuthenticated();
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -26,18 +26,21 @@
 <script setup>
 
 // http://localhost:3000/login
-const success = ref(false)
-const email = ref('')
-const pending = ref(false)
-const toast = useToast()
-const supabase = useSupabaseClient()
+const success = ref(false);
+const email = ref('');
+const pending = ref(false);
+const toast = useToast();
+const supabase = useSupabaseClient();
+
+useRedirectIfAuthenticated();
+
 const handleLogin = async () => {
     pending.value = true
     try {
         const { error } = await supabase.auth.signInWithOtp({
             email: email.value,
             options: {
-                emailRedirectTo: 'http://localhost:3000'
+                emailRedirectTo: 'http://localhost:3000/confirm'
             }
         })
         if (error) {
